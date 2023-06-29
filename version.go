f5d470a817f7623672c84b0a6852fcfae386f1a4
R. S. Doiel <rsdoiel@gmail.com>
2023-06-28 18:22:22 -0700

    Quick Save

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-06-28"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "a77fefc"
+    ReleaseHash = "4a80ca3"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
