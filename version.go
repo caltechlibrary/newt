b8b837825046b28bb6bc262e8b714ea067542e63
R. S. Doiel <rsdoiel@gmail.com>
2023-07-06 15:04:54 -0700

    presentation revisions

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-07-06"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "49b4768"
+    ReleaseHash = "b189f7a"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
