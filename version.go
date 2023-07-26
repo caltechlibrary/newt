d5c71843d3bcacc475b688e7a749ab203e64ab99
R. S. Doiel <rsdoiel@gmail.com>
2023-07-26 15:07:54 -0700

    fix: buggy SQL function generation

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-07-26"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "6f29283"
+    ReleaseHash = "c2d5924"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
