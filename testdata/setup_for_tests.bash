f46672833af6960ffffa3c5ee88063ba58e552f9
R. S. Doiel <rsdoiel@gmail>
2024-07-30 16:32:30 -0700

    Quick Save

--- testdata/setup_for_tests.bash
+++ testdata/setup_for_tests.bash
@@ -4,10 +4,10 @@ export DB_NAME="birds"
 export DB_USERNAME="birds"
 export DB_PASSWORD="my_secret_password"
 
-export NEW_ROUTES="birds-routes.csv"
+export NEW_ROUTES="birds_routes.csv"
 
 dropdb birds
 createdb birds
-psql -d birds -c '\i birds-setup.sql'
+psql -d birds -c '\i birds_setup.sql'
 pandoc server &
 postgrest postgrest.conf
--- testdata/template_engine_test.bash
+++ testdata/template_engine_test.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 
-# Launch our Mustache engine
-../bin/newtmustache -verbose -port 3032 &
+# Launch our Template Engine
+../bin/nte -verbose test_nte.yaml &
 PID=$!
 sleep 5
 if ! curl --verbose \
 		-H 'Content-Type: application/json' \
-		-d @mustache_data.json \
+		-d @template_data.json \
 		http://localhost:3032; then
 	echo "Failed to connect with curl"
 	kill "$PID"
